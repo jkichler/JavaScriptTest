0a3a3646a447365163c14bcb1936caa09af9437f
Justin Kichler <jkichler@gmail.com>
2019-10-31 13:02:10 -0400

    initial commit

--- src/analytics.js
+++ src/analytics.js
@@ -1,34 +1,31 @@
-import https from 'https'
+import https from "https";
 
 /**
  * @property {string|HistoryResponse} source
  */
 export default class Analytics {
-    constructor(source) {
-        this.source = source
-    }
+  constructor(source) {
+    this.source = source;
+  }
 
-    /**
-     * Get events history.
-     *
-     * @returns {Promise<HistoryResponse>}
-     */
-    async getHistory() {
-    }
+  /**
+   * Get events history.
+   *
+   * @returns {Promise<HistoryResponse>}
+   */
+  async getHistory() {}
 
-    /**
-     * Get total amount of all validated purchases.
-     *
-     * @returns {Promise<number>}
-     */
-    async getTotalPurchasesAmount() {
-    }
+  /**
+   * Get total amount of all validated purchases.
+   *
+   * @returns {Promise<number>}
+   */
+  async getTotalPurchasesAmount() {}
 
-    /**
-     * Get list of clients who did at least 1 validated purchase.
-     *
-     * @returns {Promise<string[]>}
-     */
-    async getClients() {
-    }
+  /**
+   * Get list of clients who did at least 1 validated purchase.
+   *
+   * @returns {Promise<string[]>}
+   */
+  async getClients() {}
 }
